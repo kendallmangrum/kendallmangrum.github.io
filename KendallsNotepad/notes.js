ac4fcc590d5609f69a13980571dbc2e4c1b0a39a
kendallmangrum <klmangrum17@gmail.com>
2021-07-21 10:41:08 -0400

    Added info about android projects

---
+++ KendallsNotepad/notes.js
@@ -0,0 +1,16 @@
+const notelist = [
+    {
+        id: '0',
+        name: 'Welcome to my Notepad!',
+        content: '',
+        date: 'July 22nd, 2021', 
+        signature: '- Kendall'
+    },
+    {
+        id: '1',
+        name: '',
+        content: '',
+        date: '',
+        signature: '- Kendall'
+    }
+]
