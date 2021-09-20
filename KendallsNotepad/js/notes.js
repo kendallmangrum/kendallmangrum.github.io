90888b241f0a2d302f96cf709d26adead1e332cc
kendallmangrum <klmangrum17@gmail.com>
2021-09-19 22:52:03 -0400

    Fist notes completed.

--- KendallsNotepad/js/notes.js
+++ KendallsNotepad/js/notes.js
@@ -22,12 +22,39 @@ const noteList = [
     },
     {
         id: '1',
-        name: '',
-        date: '',
-        preview: '',
-        content: '',
-        catchphrase: '',
+        name: 'August/Septemer Songs',
+        date: 'September 18, 2021',
+        preview: 'Here are some songs that I have been listening to on repeat that were released in August and September!',
+        content: `August and September have been some great months in terms of new music. We received new albums from the likes of Blackbear, Kanye, Drake, Lil Nas X, and several
+        other big name artists. For this edition of my notepad I am going to mention songs from each of these artists (besides Drake) that I find myself really enjoying currently.
+        <br>
+        <br>
+        The first song that I want to mention is <i>ghost town</i> by Blackbear feat. Sasha Alex Sloan from his album/ep <i>Misery Lake</i>. I have to say that I am a big Blackbear fan and enjoy several of Sasha Sloan's songs, and
+        this combination of the two doesn't disappoint. Sasha on the chorus is so catchy and Blackbear delivers his typical style of talking about the pain and lack of love that he is 
+        receiving from his significant other. If you like Blackbear's typical sound than this song will fit right in.
+        <br>
+        <br>
+        The next song that I have been constantly listening to is <i>New Again</i> by Kanye West off of his new album <i>Donda</i>. While I don't necessarily believe that this is the best song off of the album, the 
+        beginning of the song just puts me in a good mood. Kanye starts the song by saying "If I hit you with a wyd, you better not hit me with a hey. It better be like hiiiiii with a bunch of i's or
+        heyyyy with a bunch of y's." Hearing the way Kanye says this as well as the upbeat beat to the song really makes for an uplifting feeling. It also gives off positive vibes because of the church 
+        choir that is used throughout the song. If you want to hear an upbeat encouraging song by one of the greatest music producers, check this one out.
+        <br>
+        <br>
+        One of the most recently released songs that I really enjoy is <i>LOST IN THE CITADEL</i> by Lil Nas X from his debut album <i>MONTERO</i>. This song is one of the more light songs on the album
+        as it makes use of more simplistic drum and eletric guitar sounds. It is also a song that showcases Lil Nas X's voice from a singing perspective. The song has some emotion to it with the lyrics 
+        being somewhat sad while the music has a more positive tone to it. For those who don't like explicit music, this song is completely family friendly. 
+        <br>
+        <br>
+        *BONUS SONG*
+        <br>
+        There is one more song that I have to mention because it is actually the song that I have listened to the most. Some of you might not be familiar with the artist because he is rather new in the scene,
+        but if you spend any time on YouTube, Twitter, or Twitch there is a good chance you've heard of him. I'm talking about CORPSE of course. His newest song <i>HOT DEMON B!TCHES NEAR U!!!</i> with Night Lovell
+        is one that I have played back to back to back on numerous occassions since it was released. The smooth voice of Night Lovell, the hard hitting beat, one-liner verses, and simple chorus by CORPSE just gets 
+        me to nod my head and get pumped up a bit. It also follows the trick of being a short song so that you have to replay the song to make you feel like you got your full fill. As you could infer from the title
+        this song is not family friendly for several reasons, but as the kids say nowadays "this song slaps".
+        `,
+        catchphrase: 'Hope you have enjoyed your time inside my mind.',
         signature: '- Kendall',
-        page: ''
+        page: 'august-september-songs'
     }
 ]
